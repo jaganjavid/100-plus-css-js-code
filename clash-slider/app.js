8fe078da923b65c1987a0d484c1552d23b177c8b
Jagan <110019353+jaganjavid@users.noreply.github.com>
2022-08-24 15:50:25 +0530

    Add files via upload

---
+++ clash-slider/app.js
@@ -0,0 +1,21 @@
+(function() {
+  
+    var slideContainer = $('.slide-container');
+    
+    slideContainer.slick();
+    
+    $('.clash-card__image img').hide();
+    $('.slick-active').find('.clash-card img').fadeIn(200);
+    
+    // On before slide change
+    slideContainer.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+      $('.slick-active').find('.clash-card img').fadeOut(1000);
+    });
+    
+    // On after slide change
+    slideContainer.on('afterChange', function(event, slick, currentSlide) {
+      $('.slick-active').find('.clash-card img').fadeIn(200);
+    });
+    
+})();
+  
